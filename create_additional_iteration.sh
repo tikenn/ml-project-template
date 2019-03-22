ec84542635f6e810efe133330c619c5a67d78da8
Timothy I. Kennell <tikenn@uab.edu>
2019-03-22 16:41:19 -0500

    create new iteration script

---
+++ create_additional_iteration.sh
@@ -0,0 +1,6 @@
+# Training directories for iterating through machine learning process
+[[ -d "$PROJECT_PARENT_FOLDER/$PROJECT_NAME/train/iteration 1/feature_exploration" ]] || mkdir -p "$PROJECT_PARENT_FOLDER/$PROJECT_NAME/train/iteration 1/feature_exploration" 
+[[ -d "$PROJECT_PARENT_FOLDER/$PROJECT_NAME/train/iteration 1/log" ]] || mkdir -p "$PROJECT_PARENT_FOLDER/$PROJECT_NAME/train/iteration 1/log" 
+[[ -d "$PROJECT_PARENT_FOLDER/$PROJECT_NAME/train/iteration 1/results" ]] || mkdir -p "$PROJECT_PARENT_FOLDER/$PROJECT_NAME/train/iteration 1/results" 
+[[ -d "$PROJECT_PARENT_FOLDER/$PROJECT_NAME/train/iteration 1/visualizations" ]] || mkdir -p "$PROJECT_PARENT_FOLDER/$PROJECT_NAME/train/iteration 1/visualizations" 
+[[ -d "$PROJECT_PARENT_FOLDER/$PROJECT_NAME/train/iteration 1/model" ]] || mkdir -p "$PROJECT_PARENT_FOLDER/$PROJECT_NAME/train/iteration 1/model" 
